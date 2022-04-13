c44a09f01fc4d7ba75f1fc9377288a9b5590a219
Dean Ralph <dev@deanralph.com>
2022-04-13 16:57:52 +0100

    added openssl certificate generator

---
+++ openssl.py
@@ -0,0 +1,32 @@
+import os
+
+os.system('mkdir /home/dean/ssl')
+
+varFQDN = input("Please enter FQND: ")
+varIP = input("Please enter IP address: ")
+varFilename = input("Please enter filename: ")
+
+varConfigText = f"""[req]
+distinguished_name = req_distinguished_name
+req_extensions = req_ext
+prompt = no
+
+[req_distinguished_name]
+C   = GB
+ST  = Yorkshire
+L   = Bradford
+O   = DeanRalphNet
+OU  = Systems
+CN  = {varFQDN}
+
+[req_ext]
+subjectAltName = @alt_names
+
+[alt_names]
+IP.1 = {varIP}
+DNS.1 = {varFQDN}"""
+
+with open('san.cnf', 'w') as f:
+    f.write(varConfigText)
+
+os.system(f"openssl req -out {varFilename}.csr -newkey rsa:2048 -nodes -keyout {varFilename}.key -config san.cnf")
